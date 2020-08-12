e0f99347b791da270c1f0d2676bb56d8e4badb33
ecotarobles <64188994+ecotarobles@users.noreply.github.com>
2020-08-12 11:32:23 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2000 Erik Cota-Robles
+//
+// You should have received a copy of the Unlicense along with this program.
+// For more information, please refer to <http://unlicense.org/>.
+//
+#include <iostream>
+using std;
+
+int main(int argc, char **argv) {
+  cout << "Hello, Cruel World!" << endl;
+  
+  return (argc - 1);
+}
